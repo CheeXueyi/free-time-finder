88067c1617f31a1bbfc49d31c17cd2d2be1f514f
CheeXueyi <xueyi.chee@gmail.com>
2022-06-28 14:28:09 +0800

    -added comments for readability

--- webapp/main.py
+++ webapp/main.py
@@ -1,15 +1,26 @@
-from argparse import REMAINDER
-from flask import Flask, redirect, url_for, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import backref
 import datetime
 from werkzeug.exceptions import HTTPException
 
+
+
+#-----------------------------------------------------#
+#----------Web app configuration starts here----------#
+#-----------------------------------------------------#
 app = Flask(__name__)
 app.secret_key = "major_software_project"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#---------------------------------------------------#
+#----------Web app configuration ends here----------#
+#---------------------------------------------------#
 
+
+
+#----------------------------------------------#
+#----------Database models start here----------#
+#----------------------------------------------#
 db = SQLAlchemy(app)
 
 class Event(db.Model):
@@ -36,25 +47,38 @@ class Busy_date(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date=db.Column(db.Date)
     person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+#--------------------------------------------#
+#----------Database models end here----------#
+#--------------------------------------------#
 
 
-@app.route("/")
+
+#---------------------------------------#
+#----------Routing starts here----------#
+#---------------------------------------#
+#home page
+@app.route("/") 
 def home():
     return render_template("index.html")
 
-#everything under functions start here
 
-@app.route("/functions", methods=["GET"])
+#-----functions start-----#
+#main functions page
+@app.route("/functions", methods=["GET"]) 
 def functions():
     return render_template("functions.html")
 
-@app.route("/make_event", methods = ["GET", "POST"])
+
+#make new event page
+@app.route("/make_event", methods = ["GET", "POST"]) 
 def make_event():
-    #creates a new "Event" in database using inputted data and shows success page
-    if request.method == "POST":
+    #when web form is submitted, creates event according to form details
+    if request.method == "POST": 
         title = request.form['title']
         new_event = Event(title=title)
-        for i in request.form:
+        
+        #adds all dates starting from starting date to ending date to event in database
+        for i in request.form: 
             if i[:-1] == "date_range_start":
                 start = datetime.datetime.strptime(request.form[i], "%Y-%m-%d").date()
             elif i[:-1] == "date_range_end":
@@ -65,25 +89,34 @@ def make_event():
                     start += datetime.timedelta(days=1)
                 db.session.commit()
         return render_template("make_event_status.html", id=new_event.id, status = "Success", title=new_event.title)
-    else:
+    
+    #when make new event page is accessed, show make event page
+    else: 
         return render_template("make_event.html")
 
+
+#find event page
 @app.route("/find_event")
 def find_event():
-    #directs to event details page according to inputted event_id
-    if request.args.get("event_id") != None:
+    #if event_id query was set in address, show event page
+    if request.args.get("event_id") != None: 
         event_id = request.args.get("event_id")
         event = Event.query.filter_by(id=event_id).first()
         return render_template("event_page.html", event=event, n_people=len(event.people))
-    else:
+
+    #if event_id query was not set in address, show find event page
+    else: 
         return render_template("find_event.html")
 
+
+#add new participant page
 @app.route("/add_new_participant", methods=["GET", "POST"])
 def add_new_participant():
-    #creates a new "Person" in an "Event" in data base along with their busy dates
     event_id = request.args.get("event_id")
     query_event = Event.query.filter_by(id=event_id).first()
-    if request.method == "POST":
+
+    #when webform is submitted, create new participant according to form details
+    if request.method == "POST": 
         new_name = request.form["name"]
         new_person = Person(name=new_name, event=query_event)
         db.session.add(new_person)
@@ -94,14 +127,20 @@ def add_new_participant():
                 db.session.add(new_busy_date)
         db.session.commit()
         return render_template("add_new_participant_status.html", status="Success", person=new_person)
-    else:
+    
+    #when webform is not submitted, shows add new participant page 
+    else: 
         return render_template("add_new_participant.html", event=query_event)
 
+
+#edit participant page
 @app.route("/edit_participant", methods=["GET", "POST"])
 def edit_participant():
     person_id = request.args.get("person_id")
     query_person = Person.query.filter_by(id=person_id).first()
     query_event = query_person.event
+
+    #when webform is submitted, delete original participant and create new participant with new details from form 
     if request.method == "POST":
         db.session.delete(query_person)
         new_name = request.form["name"]
@@ -114,12 +153,16 @@ def edit_participant():
                 db.session.add(new_busy_date)
         db.session.commit()
         return render_template("edit_participant_status.html", status="Success", person=new_person)
+    
+    #when webform is not submitted, show edit participant page with participant's details 
     else:
         person_busy_dates = []
         for i in query_person.busy_dates:
             person_busy_dates.append(i.date)
         return render_template("edit_participant.html", person=query_person, person_busy_dates=person_busy_dates)
 
+
+#delete person activator, deletes specified participant when accessed
 @app.route("/delete_person")
 def delete_person():
     person_id = request.args["person_id"]
@@ -131,17 +174,22 @@ def delete_person():
     return render_template("delete_person_status.html", status="Success", event = event, person_name=name)
     
 
+#find best date page
 @app.route("/find_best_dates")
 def find_best_date():
     if "id" not in request.args: return "Error <a href='/'>go back to home page</a>"
     event_id = request.args["id"]
     return render_template("find_best_dates.html", event_id=event_id)
     
+
+#JSON request directory, returns event details in JSON format
 @app.route("/json")
 def json_req():
     if "id" not in request.args: return "Error <a href='/'>go back to home page</a>"
     id=request.args["id"]
     event = Event.query.get(int(id))
+
+    #if event with specified id exists
     if event != None:
         res = event.as_dict()
         
@@ -166,18 +214,24 @@ def json_req():
         #return event dict
         res["status"] = 1
         return jsonify(res)
+
+    #if event with specified id does not exist
     else:
         return jsonify({"status":0})
+#-----functions end-----#
 
-#tutorial starts here
+
+#tutorial page
 @app.route("/tutorial")
 def tutorial():
     return render_template("tutorial.html")
 
-#about us starts here
+
+#about us page
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 #error handling
 @app.errorhandler(Exception)
@@ -189,11 +243,18 @@ def handle_exception(error):
     # now you're handling non-HTTP exceptions only
     return render_template("500_generic.html", error=error)
 
-#loading database
+
+#loading database on first website visit after restarts
 @app.before_first_request
 def create_tables():
     db.create_all()
-    
+#------------------------------------#
+#----------Routing end here----------#
+#------------------------------------#
+
+
+
+#only for testing, app is run in configuration file in webhosting site
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
     
